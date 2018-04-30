89070377966995aaacf0b8e9462524470cdef948
Bianca Forbeci <biaformic@hotmail.com>
2018-04-30 15:48:48 -0300

    Cadastro simples de pessoa, com nome e email

---
+++ ExemploCadastroASP/Contexto/MeuContexto.cs
@@ -0,0 +1,23 @@
+﻿using ExemploCadastroASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ExemploCadastroASP.Contexto
+{
+    public class MeuContexto : DbContext
+    {
+        public System.Data.Entity.DbSet<ExemploCadastroASP.Models.Pessoa> Pessoas { get; set; }
+
+        public MeuContexto() : base("strConn")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<MeuContexto>());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) { modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); }
+
+    }
+}
